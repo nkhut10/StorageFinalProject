e5f60ec1ec971965d15929c65905cf1fece0f4ae
giorgi kervalishvili <gkerv10@freeuni.edu.ge>
2013-07-09 23:04:04 +0400

    operatori kategoriis
    categoriebis chveneba da damaateba baazashi

---
+++ StorageFinalProject/Controllers/StorageOperatorController.cs
@@ -0,0 +1,30 @@
+﻿using StorageFinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StorageFinalProject.Controllers
+{
+    public class StorageOperatorController : Controller
+    {
+        //
+        // GET: /StorageOperator/
+
+        private FinalProjectEntities db = new FinalProjectEntities();
+
+
+        public ActionResult Category()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        public ActionResult Create(Category category)
+        {
+            return View(new Category());
+        }
+
+
+    }
+}
