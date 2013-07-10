7c12f46b49ddf128d195e532117711d42450701f
Nika Khutsishvili <nkhut10@freeuni.edu.ge>
2013-07-10 23:03:53 +0400

    მაღაზიის ოპერატორი (ჩონჩხი)
    გავაკეთე გვერდის ჩონჩხი (გაყიდვები, ისტორია), რომელიც შესაბამის
    გვერდებზე უშვებს მაღაზიის ოპერატორს.

---
+++ StorageFinalProject/Controllers/ShopOperatorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StorageFinalProject.Controllers
+{
+    public class ShopOperatorController : Controller
+    {
+        //
+        // GET: /ShopOperator/
+
+        public ActionResult ShowMenu()
+        {
+            return View();
+        }
+
+        public ActionResult Sales()
+        {
+            return View();
+        }
+
+        public ActionResult History()
+        {
+            return View();
+        }
+
+    }
+}
