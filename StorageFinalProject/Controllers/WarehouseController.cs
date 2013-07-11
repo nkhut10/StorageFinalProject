37328187168469f64c4bbe3f2fdcd7fd0bc77b0a
lmagh10 <lmagh10@freeuni.edu.ge>
2013-07-12 03:24:01 +0400

    საწყობის მართვა vol1

---
+++ StorageFinalProject/Controllers/WarehouseController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StorageFinalProject.Models;
+
+namespace StorageFinalProject.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        private FinalProjectEntities db = new FinalProjectEntities();
+
+        //
+        // GET: /Warehouse/
+
+        public ActionResult Index()
+        {
+            return View(db.Warehouses.ToList());
+        }
+
+        //
+        // GET: /Warehouse/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Warehouse warehouse = db.Warehouses.Find(id);
+            if (warehouse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(warehouse);
+        }
+
+        //
+        // GET: /Warehouse/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Warehouse/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Warehouse warehouse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Warehouses.Add(warehouse);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(warehouse);
+        }
+
+        //
+        // GET: /Warehouse/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Warehouse warehouse = db.Warehouses.Find(id);
+            if (warehouse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(warehouse);
+        }
+
+        //
+        // POST: /Warehouse/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Warehouse warehouse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(warehouse).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(warehouse);
+        }
+
+        //
+        // GET: /Warehouse/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Warehouse warehouse = db.Warehouses.Find(id);
+            if (warehouse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(warehouse);
+        }
+
+        //
+        // POST: /Warehouse/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Warehouse warehouse = db.Warehouses.Find(id);
+            db.Warehouses.Remove(warehouse);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
