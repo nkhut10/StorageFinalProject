a88ed274311775d65287b402bbed6225065651ad
Nika Khutsishvili <nkhut10@freeuni.edu.ge>
2013-07-12 21:52:13 +0400

    დ
    დ

---
+++ StorageFinalProject/Models/SaleForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StorageFinalProject.Models
+{
+    public class SaleForm
+    {
+        [Required]
+        [Display(Name = "პროდუქტის კატეგორია")]
+        public string Category { get; set; }
+
+        [Required]
+        [Display(Name = "პროდუქტის ტიპი")]
+        public string ProductType { get; set; }
+
+        [Required]
+        [Display(Name = "საწყობის ნომერი")]
+        public bool WarehouseID { get; set; }
+
+    }
+}
